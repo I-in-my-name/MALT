c47854b87992089ca825e240d839ef090d11cb14
I-in-my-name <jack.e.d.llewellyn@gmail.com>
2025-11-11 12:43:53 +0000

    two I2S channels formed, currently using only 2 microphones but working

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -1,110 +1,196 @@
-/* Blink Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+// Include I2S driver
+#include "driver/i2s_std.h"
+#include <math.h>
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
+// Connections to INMP441 I2S microphone
+#define EXAMPLE_I2S_DUPLEX_MODE 1
+
+#define EXAMPLE_STD_MCLK_IO1 19
+#define EXAMPLE_STD_BCLK_IO1 19 // I2S bit clock io number
+#define EXAMPLE_STD_WS_IO1 18   // I2S word select io number
+#define OUT1 25                 // I2S data out io number
+#define OUT2 25                 // I2S data out io number
+#define IN1 32                  // I2S data in io number
+#define IN2 33                  // I2S data in io number
+
+#define EXAMPLE_BUFF_SIZE 2048
+
+static i2s_chan_handle_t chan1; // I2S rx channel handler
+static i2s_chan_handle_t chan2; // I2S rx channel handler
+static i2s_chan_handle_t chan3; // I2S rx channel handler
+static i2s_chan_handle_t chan4; // I2S rx channel handler
+
+// Define input buffer length
+#define bufferLen 64
+int16_t sBuffer_ALPHA[bufferLen];
+int16_t sBuffer_BETA[bufferLen];
+
+// --- I2S Configuration Functions (No changes needed here) ---
+/*
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/i2s_std.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-
+#include "esp_check.h"
 #include "sdkconfig.h"
 
-#include "driver/i2s_std.h"
+// Note: MCLK is defined but is NOT used in the final configuration to resolve the GPIO conflict.
+// If you intended to use MCLK, it MUST be on a different pin than BCLK (19).
+#define EXAMPLE_I2S_DUPLEX_MODE 1
 
-static const char *TAG = "example";
+#define EXAMPLE_STD_MCLK_IO1 19
+#define EXAMPLE_STD_BCLK_IO1 19 // I2S bit clock io number
+#define EXAMPLE_STD_WS_IO1 18   // I2S word select io number
+#define OUT1 14                 // I2S data out io number
+#define EXAMPLE_STD_DIN_IO1 33  // I2S data in io number
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO 2
+#define EXAMPLE_BUFF_SIZE 2048
 
-static uint8_t s_led_state = 0;
+static i2s_chan_handle_t chan1; // I2S tx channel handler
+static i2s_chan_handle_t chan2; // I2S tx channel handler
+static i2s_chan_handle_t chan3; // I2S tx channel handler
+static i2s_chan_handle_t chan4; // I2S tx channel handler
 
-#ifdef CONFIG_BLINK_LED_STRIP
+// --- Removed i2s_example_read_task() and merged logic into app_main ---
 
-static led_strip_handle_t led_strip;
-
-static void blink_led(void)
+static void i2s_example_init_std_duplex(void)
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state)
-    {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    }
-    else
-    {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
-    }
-}
+    /* Step 1: Determine the I2S channel configuration and allocate both channels */
+    i2s_chan_config_t pair1 = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    i2s_chan_config_t pair2 = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_SLAVE);
+    ESP_ERROR_CHECK(i2s_new_channel(&pair1, &chan1, &chan2));
+    ESP_ERROR_CHECK(i2s_new_channel(&pair2, &chan3, &chan4));
 
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
+    /* Step 2: Setting the configurations of standard mode, and initialize rx & tx channels */
+    i2s_std_config_t std_cfg1 = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(16000),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+        .gpio_cfg = {
+            // FIX 1: MCLK was set to 19 (same as BCLK). Set to UNUSED.
+            .mclk = I2S_GPIO_UNUSED,
+            .bclk = EXAMPLE_STD_BCLK_IO1,
+            .ws = EXAMPLE_STD_WS_IO1,
+            .dout = OUT1,
+            // FIX 2: DIN was set to UNUSED. Use the defined DIN pin (33).
+            .din = IN1,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv = false,
+            },
+        },
     };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
+
+    i2s_std_config_t std_cfg2 = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(16000),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+        .gpio_cfg = {
+            // FIX 1: MCLK was set to 19 (same as BCLK). Set to UNUSED.
+            .mclk = I2S_GPIO_UNUSED,
+            .bclk = EXAMPLE_STD_BCLK_IO1,
+            .ws = EXAMPLE_STD_WS_IO1,
+            .dout = OUT2,
+            // FIX 2: DIN was set to UNUSED. Use the defined DIN pin (33).
+            .din = IN2,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv = false,
+            },
+        },
     };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
+
+    /* Initialize the channels */
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(chan1, &std_cfg1));
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(chan2, &std_cfg1));
+
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(chan3, &std_cfg2));
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(chan4, &std_cfg2));
 }
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#else
-#error "unsupported LED type"
-#endif
 
 void app_main(void)
 {
+    i2s_example_init_std_duplex();
 
-    /* Configure the peripheral according to the LED type */
-    configure_led();
+    // --- Variables for I/O (Read) ---
+    uint8_t *buf1 = (uint8_t *)calloc(1, EXAMPLE_BUFF_SIZE);
+    assert(buf1);
+    size_t byte1 = 0;
 
+    uint8_t *buf2 = (uint8_t *)calloc(1, EXAMPLE_BUFF_SIZE);
+    assert(buf2);
+    size_t byte2 = 0;
+
+    uint8_t *buf3 = (uint8_t *)calloc(1, EXAMPLE_BUFF_SIZE);
+    assert(buf3);
+    size_t byte3 = 0;
+
+    uint8_t *buf4 = (uint8_t *)calloc(1, EXAMPLE_BUFF_SIZE);
+    assert(buf4);
+    size_t byte4 = 0;
+
+    // Step 3: Enable TX and RX channels
+    ESP_ERROR_CHECK(i2s_channel_enable(chan1));
+    ESP_ERROR_CHECK(i2s_channel_enable(chan2));
+    ESP_ERROR_CHECK(i2s_channel_enable(chan3));
+    ESP_ERROR_CHECK(i2s_channel_enable(chan4));
+
+    printf("I2S Duplex Loop Running without FreeRTOS Tasks...\n");
+
+    /* Step 4: Run the continuous I/O loop in app_main */
     while (1)
     {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        // --- Receive Operation ---
+        if (i2s_channel_read(chan1, buf1, EXAMPLE_BUFF_SIZE, &byte1, 100) == ESP_OK)
+        {
+            printf("chan1 %d bytes\n-----------------------------------\n", byte1);
+            printf("[0] %x [1] %x [2] %x [3] %x\n[4] %x [5] %x [6] %x [7] %x\n\n",
+                   buf1[0], buf1[1], buf1[2], buf1[3], buf1[4], buf1[5], buf1[6], buf1[7]);
+        }
+        else
+        {
+            printf("chan1 down");
+        }
+        if (i2s_channel_read(chan2, buf2, EXAMPLE_BUFF_SIZE, &byte2, 100) == ESP_OK)
+        {
+            printf("chan2 %d bytes\n-----------------------------------\n", byte2);
+            printf("[0] %x [1] %x [2] %x [3] %x\n[4] %x [5] %x [6] %x [7] %x\n\n",
+                   buf2[0], buf2[1], buf2[2], buf2[3], buf2[4], buf2[5], buf2[6], buf2[7]);
+        }
+        else
+        {
+            printf("chan2 down");
+        }
+        if (i2s_channel_read(chan3, buf3, EXAMPLE_BUFF_SIZE, &byte3, 100) == ESP_OK)
+        {
+            printf("chan3 %d bytes\n-----------------------------------\n", byte3);
+            printf("[0] %x [1] %x [2] %x [3] %x\n[4] %x [5] %x [6] %x [7] %x\n\n",
+                   buf3[0], buf3[1], buf3[2], buf3[3], buf3[4], buf3[5], buf3[6], buf3[7]);
+        }
+        else
+        {
+            printf("chan3 down");
+        }
+        if (i2s_channel_read(chan4, buf4, EXAMPLE_BUFF_SIZE, &byte4, 100) == ESP_OK)
+        {
+            printf("chan4 %d bytes\n-----------------------------------\n", byte4);
+            printf("[0] %x [1] %x [2] %x [3] %x\n[4] %x [5] %x [6] %x [7] %x\n\n",
+                   buf4[0], buf4[1], buf4[2], buf4[3], buf4[4], buf4[5], buf4[6], buf4[7]);
+        }
+        else
+        {
+            printf("chan4 down");
+        }
+
+        // Delay to yield and prevent log spamming
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
